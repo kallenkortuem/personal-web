52f1d1c8c0655e0a6381b41857129b8ad07c2cac
Kallen Kortuem <kallenjkortuem@gmail.com>
2024-10-18 20:07:42 -0500

    edit home and landing page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { DarkModeToggle } from "@/components/DarkModeToggle";
 import ProfileMenu from "@/components/ProfileMenu";
 import { getSession } from "@auth0/nextjs-auth0";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await getSession();
@@ -9,12 +10,12 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[auto_20px_1fr_20px] items-center justify-items-center min-h-screen  pb-20 gap-16  font-[family-name:var(--font-geist-sans)]">
       <header className="w-full p-4 flex items-center justify-between fixed top-0 left-0 right-0">
-        <h1 className="text-xl font-bold">My App</h1>
+        <div></div>
         <div className="flex flex-row gap-4 items-center">
           <DarkModeToggle />
           {!session && (
             <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-1 sm:px-6 sm:min-w-44"
+              className="rounded-lg border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 px-1 sm:px-6"
               href="/api/auth/login"
             >
               Login
@@ -23,24 +24,25 @@ export default async function Home() {
           {session && <ProfileMenu />}
         </div>
       </header>
-      <main className="flex flex-col gap-8 row-start-3 items-center sm:items-start pt-24">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+      <main className="flex flex-col gap-6 row-start-3 items-center sm:items-start pt-24">
+        <div>
+          <h1 className="text-3xl sm:text-4xl font-bold text-center sm:text-left mb-2">
+            Kallen Kortuem
+          </h1>
+          <p className="text-muted-foreground">
+            Full-stack engineer, designer, and creator.
+          </p>
+        </div>
+        <ol className="">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            learn more <Link href="/about-me">about me</Link>
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="mb-2">
+            explore my <Link href="/experience">experience</Link>
+          </li>
+          <li>
+            and feel free to <Link href="/contact">contact me</Link>
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -57,7 +59,7 @@ export default async function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Employ now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 focus:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 transition-all"
@@ -65,57 +67,10 @@ export default async function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read my docs
           </a>
         </div>
       </main>
-      <footer className="row-start-4 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="rounded focus:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 transition-all flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="rounded focus:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 transition-all flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="rounded focus:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 transition-all flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
